9e60a80e378d54d910e1a56b1b3295493139ed72
leantony <chachaantony@gmail.com>
2018-04-07 23:10:15 +0300

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -23,7 +23,7 @@
             <a href="{{ url('https://packagist.org/packages/leantony/laravel-grid') }}">Packagist</a>
         </li>
         <li>
-            <a href="{{ url('https://github.com/leantony/laravel-grid/blob/master/README.MD') }}">Docs</a>
+            <a href="{{ url('https://github.com/leantony/laravel-grid/wiki') }}">Docs</a>
         </li>
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Examples <b class="caret"></b></a>
