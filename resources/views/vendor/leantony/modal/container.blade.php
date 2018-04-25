d2f25c85416a0cb50c241551ea034bcb30adad71
leantony <chachaantony@gmail.com>
2018-04-25 19:08:05 +0300

    minor update

--- resources/views/vendor/leantony/modal/container.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="modal fade" id="bootstrap_modal">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <!-- the modal content will be injected here -->
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
